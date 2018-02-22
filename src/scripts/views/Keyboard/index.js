ff0a55cb6417270a8902c077598c6225db91a58e
Alexandre Lotte <alexlotte16@gmail.com>
2018-02-22 00:11:32 -0500

    jsx keyboard

--- src/scripts/views/Keyboard/index.js
+++ src/scripts/views/Keyboard/index.js
@@ -2,21 +2,21 @@ import {h} from 'hyperapp'
 
 
 export const Keyboard = (state, actions) => (
-  h("form", { class: "keyboard " + state.keyboardMode, onsubmit: ev => { actions.keyboardSubmit(ev); return false; } }, [
-    h("div", { class: "expander " + state.keyboardStatus, onclick: () => actions.expandKeyboard(state.keyboardStatus) }, [
-      h("div", { class: "default", onclick: ev => { actions.changeKeyboardMode("default") } }, "x"),
-      h("div", { class: "text", onclick: ev => { actions.changeKeyboardMode("text") } }, "txt"),
-      h("div", { class: "link", onclick: ev => { actions.changeKeyboardMode("link") } }, "url"),
-      h("div", { class: "image", onclick: ev => { actions.changeKeyboardMode("image") } }, "pic")
-    ]),
-    h("input", { class: "title", type: "text", name: "title", placeholder: "Type something..." }),
-    h("textarea", { placeholder: "Text...", name: "text" }),
-    h("input", { class: "link", type: "text", name: "link", placeholder: "Paste link here" }),
-    h("div", { class: "image" }, [
-      h("input", { type: "file", name: "image_file", id: "image_file" }),
-      h("label", { for: "image_file" }),
-      h("input", { type: "text", name: "image_link", placeholder: "Or paste link here" })
-    ]),
-    h("button", { class: "submit", type: "submit" })
-  ])
+  <form class={"keyboard " + state.keyboardMode} onsubmit={ev => { actions.keyboardSubmit(ev); return false; }}>
+    <div class={"expander " + state.keyboardStatus} onclick={ev => actions.expandKeyboard(state.keyboardStatus)}>
+      <div class="default" onclick={ev => { actions.changeKeyboardMode("default") }}>x</div>
+      <div class="text" onclick={ev => { actions.changeKeyboardMode("text") }}>txt</div>
+      <div class="link" onclick={ev => { actions.changeKeyboardMode("link") }}>url</div>
+      <div class="image" onclick={ev => { actions.changeKeyboardMode("image") }}>pic</div>
+    </div>
+    <input type="text" name="title" placeholder="Type something..." class="title" />
+    <textarea name="text" placeholder="Type something..."></textarea>
+    <input type="text" name="link" placeholder="Paste link here" class="link" />
+    <div class="image">
+      <label for="image_file"></label>
+      <input type="file" name="image_file" id="image_file" />
+      <input type="text" name="image_link" placeholder="Or paste link here" />
+    </div>
+    <button type="submit" class="submit"></button>
+  </form>
 )
