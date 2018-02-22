22e556686ced6f0ab039855fbfe5e3fb5e8b8e3b
Alexandre Lotte <alexlotte16@gmail.com>
2018-02-21 23:56:06 -0500

    jsx user view

--- src/scripts/views/UserView/index.js
+++ src/scripts/views/UserView/index.js
@@ -4,26 +4,20 @@ import {Link} from "@hyperapp/router"
 
 
 export const UserView = ({state, actions}) => (
-  h("div", { class: "global-view" }, [
-    h("div", { class: "frame" }, [
-      h("h2", {}, state.user.username),
-      h("ul", { class: "bubbles" }, [
-        state.bubbles.map(bubbleItem),
-        h("li", {}, [
-          h("span", {}, "Create bubble")
-        ])
-      ])
-    ])
-  ])
+  <div class="global-view">
+    <div class="frame">
+      <h2>{state.user.username}</h2>
+      <ul class="bubbles">
+        {state.bubbles.map(BubbleItem)}
+        <li><span>Create bubble</span></li>
+      </ul>
+    </div>
+  </div>
 )
 
 
-const bubbleItem = (bubble) => {
-  let userCountTxt = "";
-  if (bubble.userCount) {
-    userCountTxt = " (" + bubble.userCount + ")";
-  }
-  return h("li", {}, [
-    Link({ to: "/" + bubble.name }, bubble.title + userCountTxt)
-  ])
-}
+const BubbleItem = (bubble) => (
+  <li>
+    <Link to={"/" + bubble.name}>{bubble.title + " (" + bubble.userCount + ")"}</Link>
+  </li>
+)
