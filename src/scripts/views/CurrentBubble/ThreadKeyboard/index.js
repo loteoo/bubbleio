3240f85154feda93e0e32caa105375c45f61ec3f
Alexandre Lotte <alexlotte16@gmail.com>
2018-02-24 14:11:21 -0500

    icones tempo

--- src/scripts/views/CurrentBubble/ThreadKeyboard/index.js
+++ src/scripts/views/CurrentBubble/ThreadKeyboard/index.js
@@ -59,10 +59,10 @@ export const ThreadKeyboard = (currentBubble, state, actions) =>
     return false;
   }}>
     <div class="expander" onclick={ev => ev.target.classList.contains("opened") ? ev.target.classList.remove("opened") : ev.target.classList.add("opened")}>
-      <div class="default" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "default"; ev.target.parentElement.classList.remove("opened") }}>x</div>
-      <div class="text" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "text"; ev.target.parentElement.classList.remove("opened") }}>txt</div>
-      <div class="link" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "link"; ev.target.parentElement.classList.remove("opened") }}>url</div>
-      <div class="image" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "image"; ev.target.parentElement.classList.remove("opened") }}>pic</div>
+      <div class="default" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "default"; ev.target.parentElement.classList.remove("opened") }}></div>
+      <div class="text" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "text"; ev.target.parentElement.classList.remove("opened") }}></div>
+      <div class="link" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "link"; ev.target.parentElement.classList.remove("opened") }}></div>
+      <div class="image" onclick={ev => { ev.target.parentElement.parentElement.dataset.mode = "image"; ev.target.parentElement.classList.remove("opened") }}></div>
     </div>
     <input type="text" name="title" placeholder="Type something..." class="title" />
     <textarea name="text" placeholder="Type something..."></textarea>
