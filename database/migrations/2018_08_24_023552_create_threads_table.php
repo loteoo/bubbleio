1ec25695942fde77edcfb4dfcf2ef2e5f15b79a7
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-23 22:50:18 -0400

    threads migration

--- database/migrations/2018_08_24_023552_create_threads_table.php
+++ database/migrations/2018_08_24_023552_create_threads_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBubblesTable extends Migration
+class CreateThreadsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,25 +13,31 @@ class CreateBubblesTable extends Migration
      */
     public function up()
     {
-        Schema::create('bubbles', function (Blueprint $table) {
+        Schema::create('threads', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_general_ci';
             $table->increments('id');
-            $table->string('name');
             $table->string('title');
-            $table->text('description');
-            $table->boolean('public');
-            $table->boolean('default');
-            $table->softDeletes();
+            $table->text('text');
+            $table->string('link');
+            $table->string('image');
+            $table->integer('votes');
+            $table->enum('type', ['default', 'text', 'link', 'image']);
             $table->unsignedInteger('user_id');
+            $table->unsignedInteger('bubble_id');
+            $table->softDeletes();
             $table->timestamps();
-
+            
             $table->foreign('user_id')
                   ->references('id')
                   ->on('users')
                   ->onDelete('restrict');
-
+                  
+            $table->foreign('bubble_id')
+                  ->references('id')
+                  ->on('bubbles')
+                  ->onDelete('restrict');
         });
     }
 
@@ -42,6 +48,6 @@ class CreateBubblesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bubbles');
+        Schema::dropIfExists('threads');
     }
 }
